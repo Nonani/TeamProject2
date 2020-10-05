5dd2c224e32dba02b305425f323093a61bd87b1b
leejaejin <ghkfuddks@naver.com>
2020-10-05 18:42:07 +0900

    login,registration

--- TeamProject2/src/teamproject/MainClass.java
+++ TeamProject2/src/teamproject/MainClass.java
@@ -1,38 +1,45 @@
 package teamproject;
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int numI=0;
+		int numP=0;
+		int idx;
 		UserManager um = new UserManager();
 		MovieManager mm = new MovieManager();
 		um.showAll();
 		mm.showAll();
+		Scanner scan=new Scanner(System.in);
+		// 로그인/회원가입
+		while(true) {
+			idx=0;
+			System.out.println("1. 로그인 / 2. 회원가입");
+			idx=scan.nextInt();
+			
+			if(idx==1) {
+				numI = um.checkId();
+				if(numI!=-1) {
+					numP = um.checkPwd(numI);
+					if(numP==1) {
+						System.out.println("로그인 완료");
+						break;
+					}else {
+						continue;
+					}
+				}else {
+					continue;
+				}
+			}else if(idx==2) {
+				um.registerUser();
+			}else {
+				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+				continue;
+			}
+		}
 		
-		System.out.println("1. 로그인 / 2. 회원가입");
-		//로그인 회원가입 인덱스 선택받고
-		//if 로그인 
-//		{	//로그인의 경우 아이디 비밀번호를 입력받고
-//			//아이디가 존재하지않는 경우
-//			//아이디가 존재하고 비밀번호가 일치하는 경우
-//			//아이디가 존재하나 비밀번호는 일치하지 않는 경우
-//			//함수가 필요할 경우 유저 매니저에서 메소드를 정의해서 사용하세요!
-//			//ex) um.Login(id, pwd);
-//			System.out.println("아이디를 입력하세요 : ");
-//			//
-//			System.out.println("비밀번호를 입력하세요 : ");
-//		}
-//		//else 회원가입
-//		{
-//			//user 정보 입력받아서 user 객체 생성하면서 db에 저장 
-//			//이미 존재하는 id의 경우 회원가입을 못하게 하는 처리 필수
-//			//이것도 UserManager에서 메소드로 동작하게 해주세요
-//			//ex)um.SignUp(id, pwd, name, ...);
-//		}
-		
-		
-		
-		System.out.println();
 	}
 
 }
--- TeamProject2/src/teamproject/UserManager.java
+++ TeamProject2/src/teamproject/UserManager.java
@@ -1,7 +1,7 @@
 package teamproject;
-
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class UserManager extends DBManager {
@@ -10,6 +10,7 @@ public class UserManager extends DBManager {
 	
 	public UserManager() {
 		// TODO Auto-generated constructor stub
+		
 		connectDB();
 		readDB();
 	}
@@ -34,7 +35,89 @@ public class UserManager extends DBManager {
 			}
 		}
 	}
-
+	public int checkId() {
+		System.out.println("아이디를 입력해주세요.");
+		Scanner str=new Scanner(System.in);
+		String Id=str.next();
+		int ok=-1;
+		for(int i=0;i<u_list.size();i++) {
+			if(Id.equals(u_list.get(i).getId())) {
+				System.out.println("아이디가 일치합니다.");
+				ok=i;
+				break;
+			}
+		}
+		if(ok==-1) {
+			System.out.println("존재하지 않는 아이디입니다.");
+			System.out.println("로그인/회원가입 선택화면으로 돌아갑니다.");
+		}
+		return ok;
+	}
+	
+	public int checkPwd(int num) {
+		System.out.println("비밀번호를 입력해주세요.");
+		Scanner str=new Scanner(System.in);
+		String Pwd=str.next();
+		int ok=-1;
+		if(Pwd.equals(u_list.get(num).getPwd())) {
+			System.out.println("비밀번호가 일치합니다");
+			ok=1;
+		}else{
+			System.out.println("비밀번호가 일치하지 않습니다.");
+			System.out.println("로그인/회원가입 선택화면으로 돌아갑니다.");
+		}
+		return ok;
+	}
+	
+	public void registerUser() {
+		while(true) {
+			int ok=-1;
+			Scanner scan=new Scanner(System.in);
+			System.out.println("아이디를 입력하세요");
+			String Id=scan.next();
+			for(int i=0;i<u_list.size();i++) {
+				if(Id.contentEquals(u_list.get(i).getId())){
+					System.out.println("아이디 중복! 다른 아이디를 입력하세요");
+					ok=1;
+					break;
+				}
+			}
+			
+			if(ok==1) {
+				continue;
+			}
+			System.out.println("비밀번호를 입력하세요");
+			String Pwd=scan.next();
+			System.out.println("비밀번호 확인을 위해 다시 입력해주세요");
+			String Pwd2=scan.next();
+			if(!Pwd.contentEquals(Pwd2)) {
+				System.out.println("비밀번호가 일치하지 않습니다.");
+				continue;
+			}
+			System.out.println("이름 전화번호 나이를 순서대로 입력해주세요");
+			String name=scan.next();
+			String phone=scan.next();
+			int age=scan.nextInt();
+			String sql="insert into user values(?,?,?,?,?)";
+			if(conn!=null) {
+				try {
+					pstmt=conn.prepareStatement(sql);
+					pstmt.setString(1, Id);
+					pstmt.setString(2, Pwd);
+					pstmt.setString(3, name);
+					pstmt.setString(4, phone);
+					pstmt.setInt(5, age);
+					pstmt.execute();
+				} catch(SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			showAll();
+			break;
+		}
+	}
+	
 	@Override
 	public void showAll() {
 		// TODO Auto-generated method stub
